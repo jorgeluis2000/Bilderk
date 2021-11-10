41a0932c013fe5bb241ca90430ea4dfcabd8e2de
jorgeluis2000 <67931299+jorgeluis2000@users.noreply.github.com>
2021-11-09 19:00:19 -0500

    Delete recognition.py

--- src/recognition.py
+++
@@ -1,9 +0,0 @@
-from src.interface import IClaseAdstrac
-
-class FaceRecognition(IClaseAdstrac):
-    def __init__(self):
-        super(FaceRecognition, self).__init__()
-        IClaseAdstrac.__init__(self)
-
-    def who_are_you(self):
-        print('''I'm a Face Recognition''' + '\ncasa')
